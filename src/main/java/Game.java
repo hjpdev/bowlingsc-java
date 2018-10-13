b7488bfc14b3ee32656a618ad26253f71a34358c
Harry <harryjp1995@gmail.com>
2018-10-13 15:07:20 +0100

    feat: Second test passes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,11 +1,13 @@
 package main.java;
 
 public class Game {
-  public void roll(int pins) {
+  private int score;
 
+  public void roll(int pins) {
+    score += pins;
   }
 
   public int score() {
-    return 0;
+    return score;
   }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -14,19 +14,21 @@ public class GameTest {
     game = new Game();
   }
 
+  private void rollManySame(int n, int pins) {
+    for (int i = 0; i < n; i++) {
+      game.roll(pins);
+    }
+  }
+
   @Test
   public void gutterGame() {
-    for (int i = 0; i < 20; i++) {
-      game.roll(0);
-    }
+    rollManySame(20, 0);
     assertEquals(0, game.score());
   }
 
   @Test
   public void allOnes() {
-    for (int i = 0; i < 20; i++) {
-      game.roll(1);
-    }
+    rollManySame(20, 1);
     assertEquals(20, game.score());
   }
 }
